ca62be879ab1a5f0977f14bed10f613d65be4fb4
lovestarry7788 <jay.lau2020.work@outlook.com>
2023-01-30 16:39:17 +0800

    project1_buffer_pool

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -20,10 +20,13 @@ namespace bustub {
 BufferPoolManager::BufferPoolManager(size_t pool_size, DiskManager *disk_manager, size_t replacer_k,
                                      LogManager *log_manager)
     : pool_size_(pool_size), disk_manager_(disk_manager), log_manager_(log_manager) {
-  // TODO(students): remove this line after you have implemented the buffer pool manager
+  //  TODO(students): remove this line after you have implemented the buffer pool manager
+
+  /*
   throw NotImplementedException(
       "BufferPoolManager is not implemented yet. If you have finished implementing BPM, please remove the throw "
       "exception line in `buffer_pool_manager.cpp`.");
+  */
 
   // we allocate a consecutive memory space for the buffer pool
   pages_ = new Page[pool_size_];
@@ -42,18 +45,225 @@ BufferPoolManager::~BufferPoolManager() {
   delete replacer_;
 }
 
-auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * { return nullptr; }
+/**
+   * TODO(P1): Add implementation
+   *
+   * @brief Create a new page in the buffer pool. Set page_id to the new page's id, or nullptr if all frames
+   * are currently in use and not evictable (in another word, pinned).
+   *
+   * You should pick the replacement frame from either the free list or the replacer (always find from the free list
+   * first), and then call the AllocatePage() method to get a new page id. If the replacement frame has a dirty page,
+   * you should write it back to the disk first. You also need to reset the memory and metadata for the new page.
+   *
+   * Remember to "Pin" the frame by calling replacer.SetEvictable(frame_id, false)
+   * so that the replacer wouldn't evict the frame before the buffer pool manager "Unpin"s it.
+   * Also, remember to record the access history of the frame in the replacer for the lru-k algorithm to work.
+   *
+   * @param[out] page_id id of created page
+   * @return nullptr if no new pages could be created, otherwise pointer to new page
+ */
 
-auto BufferPoolManager::FetchPage(page_id_t page_id) -> Page * { return nullptr; }
+auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * {
+  std::scoped_lock<std::mutex>lock(latch_);
 
-auto BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty) -> bool { return false; }
+  // 1. 获取空闲的 frame
+  frame_id_t frame_id = -1;
+  if(!free_list_.empty()) {
+    frame_id = free_list_.front();
+    free_list_.pop_front();
+  } else if(replacer_ -> Evict(&frame_id) == true) { // 能找到剔除的页面
+    if(pages_[frame_id].is_dirty_) {
+      disk_manager_ ->WritePage(pages_[frame_id].page_id_, pages_[frame_id].data_);
+      pages_[frame_id].is_dirty_ = false;
+    }
+    page_table_->Remove(pages_[frame_id].page_id_);
+  } else {
+    return nullptr;
+  }
 
-auto BufferPoolManager::FlushPage(page_id_t page_id) -> bool { return false; }
+  // 2. 获取 page_id
+  *page_id = AllocatePage();
+  auto page = pages_ + frame_id;
+  page->page_id_ = *page_id;
+  page->is_dirty_ = false;
+  page->pin_count_ ++;
+  replacer_->SetEvictable(*page_id, true);
+  page->ResetMemory();
+  page_table_ -> Insert(*page_id, frame_id);
 
-void BufferPoolManager::FlushAllPages() {}
+  return page;
+}
 
-auto BufferPoolManager::DeletePage(page_id_t page_id) -> bool { return false; }
+/**
+   * TODO(P1): Add implementation
+   *
+   * @brief Fetch the requested page from the buffer pool. Return nullptr if page_id needs to be fetched from the disk
+   * but all frames are currently in use and not evictable (in another word, pinned).
+   *
+   * First search for page_id in the buffer pool. If not found, pick a replacement frame from either the free list or
+   * the replacer (always find from the free list first), read the page from disk by calling disk_manager_->ReadPage(),
+   * and replace the old page in the frame. Similar to NewPage(), if the old page is dirty, you need to write it back
+   * to disk and update the metadata of the new page
+   *
+   * In addition, remember to disable eviction and record the access history of the frame like you did for NewPage().
+   *
+   * @param page_id id of page to be fetched
+   * @return nullptr if page_id cannot be fetched, otherwise pointer to the requested page
+ */
+auto BufferPoolManager::FetchPage(page_id_t page_id) -> Page * {
+  std::scoped_lock<std::mutex>lock(latch_);
 
-auto BufferPoolManager::AllocatePage() -> page_id_t { return next_page_id_++; }
+  frame_id_t frame_id = -1;
+  bool can_find = page_table_ -> Find(page_id, frame_id);
+  if(can_find == true) {
+    auto page = pages_ + frame_id;
+    page -> pin_count_ ++;
+    replacer_ ->SetEvictable(page_id, true);
+    return page;
+  }
+
+  // 2. 获取空闲的 frame
+  if(!free_list_.empty()) {
+    frame_id = free_list_.front();
+    free_list_.pop_front();
+  } else if(replacer_->Evict(&frame_id) == true) {
+    auto page = pages_ + frame_id;
+    if(page -> is_dirty_) {
+      disk_manager_ ->WritePage(page->page_id_, page->data_);
+      page -> is_dirty_ = false;
+    }
+    page_table_->Remove(page->page_id_);
+  } else {
+    return nullptr;
+  }
+
+  // 3. 获取 page_id
+  auto page = pages_ + frame_id;
+  page->page_id_ = page_id;
+  page->is_dirty_ = false;
+  page->pin_count_ ++;
+  replacer_->SetEvictable(page_id, true);
+  disk_manager_ -> ReadPage(page_id, page->data_);
+  page->ResetMemory();
+  page_table_ -> Insert(page_id, frame_id);
+
+  return page;
+}
+
+/**
+   * TODO(P1): Add implementation
+   *
+   * @brief Unpin the target page from the buffer pool. If page_id is not in the buffer pool or its pin count is already
+   * 0, return false.
+   *
+   * Decrement the pin count of a page. If the pin count reaches 0, the frame should be evictable by the replacer.
+   * Also, set the dirty flag on the page to indicate if the page was modified.
+   *
+   * @param page_id id of page to be unpinned
+   * @param is_dirty true if the page should be marked as dirty, false otherwise
+   * @return false if the page is not in the page table or its pin count is <= 0 before this call, true otherwise
+ */
+auto BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty) -> bool {
+  std::scoped_lock<std::mutex>lock(latch_);
+  frame_id_t frame_id = -1;
+  bool can_find = page_table_->Find(page_id, frame_id);
+  if(can_find == true) {
+    auto page = pages_ + frame_id;
+    if(page -> pin_count_ <= 0) {
+      return false;
+    } else if(page -> pin_count_ > 0){
+      page -> pin_count_ --;
+      if(page -> pin_count_ == 0) {
+        replacer_->SetEvictable(frame_id, true);
+        page -> is_dirty_ |= is_dirty;
+      }
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+   * TODO(P1): Add implementation
+   *
+   * @brief Flush the target page to disk.
+   *
+   * Use the DiskManager::WritePage() method to flush a page to disk, REGARDLESS of the dirty flag.
+   * Unset the dirty flag of the page after flushing.
+   *
+   * @param page_id id of page to be flushed, cannot be INVALID_PAGE_ID
+   * @return false if the page could not be found in the page table, true otherwise
+ */
+auto BufferPoolManager::FlushPage(page_id_t page_id) -> bool {
+  std::scoped_lock<std::mutex>lock(latch_);
+  frame_id_t frame_id = -1;
+  bool can_find = page_table_->Find(page_id, frame_id);
+  if(can_find == true) {
+    auto page = pages_ + frame_id;
+    disk_manager_->WritePage(page_id, page->data_);
+    page->is_dirty_ = false;
+    return true;
+  }
+  return false;
+}
+
+/**
+   * TODO(P1): Add implementation
+   *
+   * @brief Flush all the pages in the buffer pool to disk.
+ */
+void BufferPoolManager::FlushAllPages() {
+  std::scoped_lock<std::mutex>lock(latch_);
+  for(size_t i = 0; i < pool_size_; i++) {
+    if(pages_[i].page_id_ != INVALID_PAGE_ID) {
+      disk_manager_->WritePage(pages_[i].page_id_, pages_[i].data_);
+      pages_[i].is_dirty_ = false;
+    }
+  }
+}
+
+/**
+   * TODO(P1): Add implementation
+   *
+   * @brief Delete a page from the buffer pool. If page_id is not in the buffer pool, do nothing and return true. If the
+   * page is pinned and cannot be deleted, return false immediately.
+   *
+   * After deleting the page from the page table, stop tracking the frame in the replacer and add the frame
+   * back to the free list. Also, reset the page's memory and metadata. Finally, you should call DeallocatePage() to
+   * imitate freeing the page on the disk.
+   *
+   * @param page_id id of page to be deleted
+   * @return false if the page exists but could not be deleted, true if the page didn't exist or deletion succeeded
+ */
+auto BufferPoolManager::DeletePage(page_id_t page_id) -> bool {
+  std::scoped_lock<std::mutex>lock(latch_);
+  frame_id_t frame_id = -1;
+  bool can_find = page_table_->Find(page_id, frame_id);
+  if(can_find == false) {
+    return true;
+  }
+  auto page = pages_ + frame_id;
+  if(page -> pin_count_) {
+    return false;
+  }
+  if(page -> IsDirty()) {
+    page -> is_dirty_ = 0;
+    disk_manager_ ->WritePage(page_id, page->GetData());
+  }
+  page_table_->Remove(pages_[frame_id].page_id_);
+  free_list_.emplace_back(frame_id);
+  page->ResetMemory();
+  page->page_id_ = INVALID_PAGE_ID;
+  DeallocatePage(page_id);
+  return true;
+}
+
+/**
+   * @brief Allocate a page on disk. Caller should acquire the latch before calling this function.
+   * @return the id of the allocated page
+ */
+auto BufferPoolManager::AllocatePage() -> page_id_t {
+  return next_page_id_++;
+}
 
 }  // namespace bustub
